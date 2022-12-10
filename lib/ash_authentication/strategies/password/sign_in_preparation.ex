5f701ea87e56c7991b946563f3ab0e87caaf3d02
zimt28 <1764689+zimt28@users.noreply.github.com>
2022-12-10 16:15:49 +0100

    Update sign_in_preparation.ex

--- lib/ash_authentication/strategies/password/sign_in_preparation.ex
+++ lib/ash_authentication/strategies/password/sign_in_preparation.ex
@@ -30,13 +30,11 @@ defmodule AshAuthentication.Strategy.Password.SignInPreparation do
     |> Query.after_action(fn
       query, [record] ->
         password = Query.get_argument(query, strategy.password_field)
+        hashed_password = Map.get(record, strategy.hashed_password_field)
 
-        if strategy.hash_provider.valid?(
-             password,
-             Map.get(record, strategy.hashed_password_field)
-           ),
-           do: {:ok, [maybe_generate_token(record)]},
-           else: auth_failed(query)
+        if strategy.hash_provider.valid?(password, hashed_password),
+          do: {:ok, [maybe_generate_token(record)]},
+          else: auth_failed(query)
 
       _, _ ->
         strategy.hash_provider.simulate()
