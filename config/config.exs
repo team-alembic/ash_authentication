150ab139aa8e54352bfcec7a456f920a441fc420
James Harton <james@harton.nz>
2023-01-12 15:30:08 +1300

    chore: fix build.
    I'd rather have the warning than a build failure.

--- config/config.exs
+++ config/config.exs
@@ -5,7 +5,5 @@ config :mime, :types, %{
 }
 
 config :ash, :utc_datetime_type, :datetime
-# resolve temporary backwards compatibility warning in Ash
-config :ash, :use_all_identities_in_manage_relationship?, false
 
 import_config "#{config_env()}.exs"
