8070e011cd6234879e9c3b01aed9a6b5c5111b88
Zach Daniel <zach@zachdaniel.dev>
2024-04-25 22:29:04 -0400

    chore: add ash-functions for tests

--- test/support/example/repo.ex
+++ test/support/example/repo.ex
@@ -3,5 +3,5 @@ defmodule Example.Repo do
   use AshPostgres.Repo, otp_app: :ash_authentication
 
   @doc false
-  def installed_extensions, do: ["uuid-ossp", "citext"]
+  def installed_extensions, do: ["ash-functions", "uuid-ossp", "citext"]
 end
