86be412641cb0c852806ac17db9169bd9947d08b
Zach Daniel <zach@zachdaniel.dev>
2024-08-12 09:10:59 -0400

    fix: add back in accidentally removed debug errors code (#768)
    this also cleans up references to `Ash.ErrorKind` which is
    dead code in 3.x

--- lib/ash_authentication/errors/invalid_token.ex
+++ lib/ash_authentication/errors/invalid_token.ex
@@ -2,16 +2,7 @@ defmodule AshAuthentication.Errors.InvalidToken do
   @moduledoc """
   An invalid token was presented.
   """
-  use Ash.Error.Exception
   use Splode.Error, fields: [:type], class: :forbidden
 
   def message(%{type: type}), do: "Invalid #{type} token"
-
-  defimpl Ash.ErrorKind do
-    @moduledoc false
-    def id(_), do: Ecto.UUID.generate()
-    def code(_), do: "invalid_token"
-    def message(%{type: nil}), do: "Invalid token"
-    def message(%{type: type}), do: "Invalid #{type} token"
-  end
 end
--- lib/ash_authentication/errors/missing_secret.ex
+++ lib/ash_authentication/errors/missing_secret.ex
@@ -2,20 +2,9 @@ defmodule AshAuthentication.Errors.MissingSecret do
   @moduledoc """
   A secret is now missing.
   """
-  use Ash.Error.Exception
   use Splode.Error, fields: [:resource], class: :forbidden
 
   def message(%{path: path, resource: resource}) do
     "Secret for `#{Enum.join(path, ".")}` on the `#{inspect(resource)}` resource is not accessible."
-  end
-
-  defimpl Ash.ErrorKind do
-    @moduledoc false
-    def id(_), do: Ecto.UUID.generate()
-    def code(_), do: "missing_secret"
-
-    def message(%{path: path, resource: resource}),
-      do:
-        "Secret for `#{Enum.join(path, ".")}` on the `#{inspect(resource)}` resource is not accessible."
   end
 end
