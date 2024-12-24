e6e9abe0b187ef58f717745873b33928d8489782
Zach Daniel <zach@zachdaniel.dev>
2024-12-23 19:11:29 -0500

    chore: nevermind :)

--- lib/mix/tasks/ash_authentication.install.ex
+++ lib/mix/tasks/ash_authentication.install.ex
@@ -147,7 +147,7 @@ if Code.ensure_loaded?(Igniter) do
             if Code.ensure_loaded?(AshPostgres.DataLayer) do
               "postgres,#{extensions}"
             else
-              "ets,extensions"
+              extensions
             end
 
           dev_secret =
