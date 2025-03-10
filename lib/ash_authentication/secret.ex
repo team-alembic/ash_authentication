d08adb4e5951ad292fd8fdda75d05b1dcb374dea
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2025-03-10 13:46:04 +0100

    feat: add context to secret functions

--- lib/ash_authentication/secret.ex
+++ lib/ash_authentication/secret.ex
@@ -4,13 +4,17 @@ defmodule AshAuthentication.Secret do
 
   Allows you to implement secrets access via your method or choice at runtime.
 
+  The context paramter is either a map with the `conn` key containing the Plug.Conn
+  if the secret is being retrieved in a plug, or the context of the ash action it is
+  called in
+
   ## Example
 
   ```elixir
   defmodule MyApp.GetSecret do
     use AshAuthentication.Secret
 
-    def secret_for([:authentication, :strategies, :oauth2, :client_id], MyApp.User, _opts), do: Application.fetch_env(:my_app, :oauth_client_id)
+    def secret_for([:authentication, :strategies, :oauth2, :client_id], MyApp.User, _opts, _context), do: Application.fetch_env(:my_app, :oauth_client_id)
     def secret_for([:authentication, :strategies, :oauth2, :client_secret], MyApp.User, _opts), do: Application.fetch_env(:my_app, :oauth_client_secret)
   end
 
@@ -68,11 +72,42 @@ defmodule AshAuthentication.Secret do
   """
   @callback secret_for(secret_name :: [atom], Resource.t(), keyword) :: {:ok, String.t()} | :error
 
+  @doc """
+  Secret retrieval callback.
+
+  This function will be called with the "secret name", see the module
+  documentation for more info.
+
+  The context paramter is either a map with the `conn` key containing the Plug.Conn
+  if the secret is being retrieved in a plug, or the context of the ash action it is
+  called in
+  """
+  @callback secret_for(secret_name :: [atom], Resource.t(), keyword, context :: map()) ::
+              {:ok, String.t()} | :error
+
+  @optional_callbacks secret_for: 3
+
   @doc false
   @spec __using__(any) :: Macro.t()
   defmacro __using__(_) do
     quote do
       @behaviour AshAuthentication.Secret
+      @before_compile AshAuthentication.Secret
+    end
+  end
+
+  # if the `secret_for/3` callback is implemented add a adapter function that calls it
+  # without the context. This is to allow for backwards compatibility and as a nice
+  # side-effect it allows to mix and match the two callback styles, because this function
+  # is put at the end of the module if there is no other `/4` function that matches id
+  # would pass it to the `/3` function.
+  defmacro __before_compile__(_) do
+    quote do
+      if Module.defines?(__MODULE__, {:secret_for, 3}, :def) do
+        def secret_for(secret_name, resource, opts, _) do
+          secret_for(secret_name, resource, opts)
+        end
+      end
     end
   end
 end
