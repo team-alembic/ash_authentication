df69b8db33ea6975d52124f0b7cd693c82d4a436
Rebecca Le <rebecca@alembic.com.au>
2025-05-23 15:13:53 +0800

    chore(TOTP): Handle TOTP verification failures by wrapping in `AuthenticationFailed`

--- lib/ash_authentication/add_ons/two_factor_totp/plug.ex
+++ lib/ash_authentication/add_ons/two_factor_totp/plug.ex
@@ -1,15 +1,30 @@
 defmodule AshAuthentication.AddOn.TwoFactorTotp.Plug do
-  alias AshAuthentication.{AddOn.TwoFactorTotp, Strategy}
+  alias AshAuthentication.{AddOn.TwoFactorTotp, Errors.AuthenticationFailed, Strategy}
   alias Plug.Conn
 
   import AshAuthentication.Plug.Helpers, only: [store_authentication_result: 2]
 
   @spec verify(Conn.t(), TwoFactorTotp.t()) :: Conn.t()
   def verify(conn, strategy) do
-    # TODO: will the assign always be `current_user`?
     data = %{totp: conn.params["totp"], user: conn.assigns.current_user}
-    result = Strategy.action(strategy, :verify, data, [])
-    # TODO: Error handling, wrap in AuthenticationFailed
-    store_authentication_result(conn, result)
+
+    case Strategy.action(strategy, :verify, data, []) do
+      {:ok, user} ->
+        store_authentication_result(conn, {:ok, user})
+
+      {:error, reason} ->
+        error =
+          AuthenticationFailed.exception(
+            strategy: strategy,
+            caused_by: %{
+              module: __MODULE__,
+              strategy: strategy,
+              action: :verify,
+              message: "TOTP verification failed: #{inspect(reason)}"
+            }
+          )
+
+        store_authentication_result(conn, {:error, error})
+    end
   end
 end
