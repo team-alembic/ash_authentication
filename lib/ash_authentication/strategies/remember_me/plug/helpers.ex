c30ce851d2cbe406dd197bf8a50ba8d9b059bae8
Robert Graff <robert@workingdemo.com>
2025-06-21 20:42:15 +0000

    Add remember me cookie helpers

--- lib/ash_authentication/strategies/remember_me/plug/helpers.ex
+++ lib/ash_authentication/strategies/remember_me/plug/helpers.ex
@@ -5,6 +5,7 @@ defmodule AshAuthentication.Strategy.RememberMe.Plug.Helpers do
 
   alias Ash.Query
   alias AshAuthentication.{Info, Strategy.RememberMe}
+  alias Plug.Conn
 
   @doc """
   Sign in the given Ash Resource with the AshAuthentication RememberMe strategy.
@@ -66,6 +67,53 @@ defmodule AshAuthentication.Strategy.RememberMe.Plug.Helpers do
 
         _ ->
           conn
+      end
+    end)
+  end
+
+  @remember_me_cookie_options [
+    # prevents the cookie from being accessed by JavaScript
+    http_only: true,
+    # only send the cookie over HTTPS
+    secure: true,
+    # prevents the cookie from being sent with cross-site requests
+    same_site: "Lax"
+  ]
+
+  @doc """
+  Put the remember me token in connection response cookies.
+  """
+  @spec put_remember_me_cookie(Conn.t(), String.t(), Map.t()) :: Conn.t()
+  def put_remember_me_cookie(conn, cookie_name, %{token: token, max_age: max_age}) do
+    cookie_options = Keyword.put(@remember_me_cookie_options, :max_age, max_age)
+
+    conn
+    |> Conn.put_resp_cookie(cookie_name, token, cookie_options)
+  end
+
+  @doc """
+  Delete the remember me token from the connection response cookies.
+  """
+  @spec delete_remember_me_cookie(Conn.t(), String.t()) :: Conn.t()
+  def delete_remember_me_cookie(conn, cookie_name) do
+    cookie_options = Keyword.put(@remember_me_cookie_options, :max_age, 0)
+
+    conn
+    |> Conn.delete_resp_cookie(cookie_name, cookie_options)
+  end
+
+  @doc """
+  Delete all the remember me tokens from the response cookies.
+  """
+  @spec delete_all_remember_me_cookies(Conn.t()) :: Conn.t()
+  def delete_all_remember_me_cookies(conn) do
+    conn
+    |> Conn.get_cookies()
+    |> Enum.reduce(conn, fn {key, _}, conn ->
+      if String.starts_with?(key, AshAuthentication.Strategy.RememberMe.Cookie.prefix()) do
+        delete_remember_me(conn, key)
+      else
+        conn
       end
     end)
   end
