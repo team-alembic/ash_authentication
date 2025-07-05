483447582f86d43fa7b7551099ca1afe083b2840
Steffen Beyer <steffen@beyer.io>
2025-07-05 15:11:44 +0200

    improve: API key auth for multi-tenancy resources
    Introduce optional `multitenancy_relationship` to the API key strategy to
    allow the authenticated resource to be bound to a tenant.

--- lib/ash_authentication/strategies/api_key/dsl.ex
+++ lib/ash_authentication/strategies/api_key/dsl.ex
@@ -33,6 +33,11 @@ defmodule AshAuthentication.Strategy.ApiKey.Dsl do
           type: :atom,
           doc:
             "The name to use for the sign in action. Defaults to `sign_in_with_<strategy_name>`"
+        ],
+        multitenancy_relationship: [
+          type: :atom,
+          doc:
+            "The relationship from the API key to the issuing tenant, used to access the user resource. Defaults to global user resource."
         ]
       ]
     }
