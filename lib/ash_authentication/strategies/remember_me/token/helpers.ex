e1ce824f72e357debc7c9d3fba8452accd624d1d
Robert Graff <robert@workingdemo.com>
2025-09-07 17:09:39 -0700

    fixup

--- lib/ash_authentication/strategies/remember_me/token/helpers.ex
+++ lib/ash_authentication/strategies/remember_me/token/helpers.ex
@@ -11,8 +11,7 @@ defmodule AshAuthentication.Strategy.RememberMe.Token.Helpers do
   def revoke_remember_me_token(token, otp_app, opts \\ []) do
     with {:ok, resource} <- Jwt.token_to_resource(token, otp_app),
          {:ok, token_resource} <- Info.authentication_tokens_token_resource(resource) do
-      # we want this to blow up if something goes wrong
-      :ok = TokenResource.Actions.revoke(token_resource, token, opts)
+      TokenResource.Actions.revoke(token_resource, token, opts)
     end
   end
 end
